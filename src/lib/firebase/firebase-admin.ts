8baa511e6f1abeb76cfc914952dbcd162f2f03dc
juanda050 <jxuand3@gmail.com>
2025-08-09 14:20:32 +0700

    Removed Firebase-Admin SDK Package

--- src/lib/firebase/firebase-admin.ts
+++ src/lib/firebase/firebase-admin.ts
@@ -1,14 +1,14 @@
-// lib/firebase-admin.ts
-import admin from 'firebase-admin';
-import { getApps } from 'firebase-admin/app';
+// // lib/firebase-admin.ts
+// import admin from 'firebase-admin';
+// import { getApps } from 'firebase-admin/app';
 
-const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON as string);
+// const serviceAccount = JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON as string);
 
-if (getApps().length === 0) {
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-    });
-}
+// if (getApps().length === 0) {
+//     admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount),
+//     });
+// }
 
-const adminDb = admin.firestore();
-export { adminDb };
+// const adminDb = admin.firestore();
+// export { adminDb };
