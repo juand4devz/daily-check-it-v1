f2ec3ddcad9b475033d92b86b8527e941a23448f
juanda050 <jxuand3@gmail.com>
2025-08-09 14:33:50 +0700

    Removed Firebase-Admin SDK Package

--- src/lib/firebase/firebase-client.ts
+++ src/lib/firebase/firebase-client.ts
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
     apiKey: process.env.API_KEY as string,
     authDomain: process.env.AUTH_DOMAIN as string,
-    // databaseURL: process.env.DATABASE_URL as string,
+    databaseURL: process.env.DATABASE_URL as string,
     projectId: process.env.PROJECT_ID as string,
     storageBucket: process.env.STORAGE_BUCKET as string,
     messagingSenderId: process.env.MESSAGING_SENDER_ID as string,
